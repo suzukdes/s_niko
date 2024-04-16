7eb359fc8cdb16294e01961fe4540eed037d68c4
suzukdes <167181783+suzukdes@users.noreply.github.com>
2024-04-16 08:44:35 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,17 @@
 import streamlit as st
-import random
+import math
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "引き分けです！"
-    elif (user_choice == "rock" and computer_choice == "scissors") or \
-         (user_choice == "paper" and computer_choice == "rock") or \
-         (user_choice == "scissors" and computer_choice == "paper"):
-        return "あなたの勝ちです！"
-    else:
-        return "コンピューターの勝ちです！"
+def calculate_pi(num_digits):
+    return round(math.pi, num_digits)
 
 def main():
-    st.title("じゃんけんゲーム")
-    
-    user_choice = st.radio("あなたの選択:", ("rock", "paper", "scissors"))
+    st.title("円周率（π）の計算")
 
-    computer_choice = random.choice(["rock", "paper", "scissors"])
+    num_digits = st.slider("小数点以下の桁数を選択してください:", 1, 20, 5)
 
-    if st.button("結果を見る"):
-        st.write("あなたの選択:", user_choice)
-        st.write("コンピューターの選択:", computer_choice)
-        st.write(determine_winner(user_choice, computer_choice))
+    pi_value = calculate_pi(num_digits)
+
+    st.write(f"円周率（π）の値は {pi_value} です。")
 
 if __name__ == "__main__":
     main()
